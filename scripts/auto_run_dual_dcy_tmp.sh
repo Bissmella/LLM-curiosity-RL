9e00f6b57f5dab6523d13c48084df3025d0f5cb4
Bissmella Bahaduri <bissmellabahaduri@gmail.com>
2025-08-13 22:57:28 +0200

    added scripts

--- scripts/auto_run_dual_dcy_tmp.sh
+++ scripts/auto_run_dual_dcy_tmp.sh
@@ -1,13 +1,14 @@
-SEED=$1              #2
-WANDBRUN=$2        #"LLM_2"
-OUTPUT_DIR=$3      #"/home/bahaduri/VIPER/outputs/LLM_2"  #$2         #
-PORT=$4                   #12355                #$4
-TASK=1    #$1   #
-INTRINSIC_REWARD=False
-DUAL_VAL=False
+SEED=1
+WANDBRUN="LLM_tmp_dual"
+OUTPUT_DIR="/LLM-curiosity-RL/outputs/LLM_tmp_dual"
+PORT=12355
+TASK=1
+INTRINSIC_REWARD=True
+DUAL_VAL=True
 INTRINSIC_DECAY=False
+TEMP_PRED=True
 STARTEPOCHS=0
-LOADING_PATH=""
+LOADING_PATH=""   
 
 #create output_dir if not existing
 if [ ! -d "$OUTPUT_DIR" ]; then
@@ -18,26 +19,27 @@ fi
 #lamorel_args.llm_args.vlm_model_path="microsoft/Florence-2-base-ft" \
 
 python3 -m lamorel_launcher.launch \
-    --config-path "/home/bahaduri/VIPER/experiments/configs/" \
+    --config-path "/LLM-curiosity-RL/experiments/configs/" \
     --config-name "local_gpu_config" \
-    rl_script_args.path="/home/bahaduri/VIPER/experiments/Train_PPO.py" \
+    rl_script_args.path="/LLM-curiosity-RL/experiments/Train_cur_PPO.py" \
     rl_script_args.output_dir=.  \
+    lamorel_args.allow_subgraph_use_whith_gradient=True \
     lamorel_args.accelerate_args.machine_rank=0 \
     lamorel_args.llm_args.model_path="meta-llama/Llama-3.2-1B-Instruct" \
     lamorel_args.llm_args.model_type="causal" \
-    lamorel_args.llm_args.minibatch_size=164 \
-    rl_script_args.minibatch_size=164 \
     lamorel_args.accelerate_args.main_process_port="$PORT" \
+    lamorel_args.llm_args.minibatch_size=128 \
+    rl_script_args.minibatch_size=164 \
     rl_script_args.seed="$SEED" \
     rl_script_args.epochs=150 \
     rl_script_args.number_envs=1 \
     rl_script_args.task="[$TASK]" \
     rl_script_args.output_dir="$OUTPUT_DIR" \
-    rl_script_args.loading_path="$LOADING_PATH" \
     rl_script_args.intrinsic_reward="$INTRINSIC_REWARD" \
     rl_script_args.dual_val="$DUAL_VAL" \
     rl_script_args.intrinsic_decay="$INTRINSIC_DECAY" \
-    lamorel_args.config_alfred="/home/bahaduri/VIPER/alfworld/configs/base_config.yaml" \
+    rl_script_args.temp_pred="$TEMP_PRED" \
+    lamorel_args.config_alfred="/LLM-curiosity-RL/alfworld/configs/base_config.yaml" \
     wandb_args.run="$WANDBRUN" \
     wandb_args.mode="online" \
     lamorel_args.distributed_setup_args.n_llm_processes=1 \
